83c5c0a352c279d72f598514099c5a5016050944
Iván Pérez <79656478+iperezc2019urjc@users.noreply.github.com>
2021-12-21 20:06:37 +0100

    Update SalaChat.java

--- Volley Fighters/Volley/src/main/java/com/example/demo/SalaChat.java
+++ Volley Fighters/Volley/src/main/java/com/example/demo/SalaChat.java
@@ -8,14 +8,11 @@ public class SalaChat {
 List<String> mensajes = new ArrayList<String>();
 	
 	public SalaChat() {
-		//mensajes.add("---------- CHAT ----------");
 	}
 	
 	public List<String> getMensajes() {
 		return mensajes;
 	}
-
-	
 
 	public void setMensajes(List<String> readAllLines) {
 		this.mensajes = mensajes;
