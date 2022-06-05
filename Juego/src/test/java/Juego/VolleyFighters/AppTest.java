f3a0e7a2e61e1303a245eca5c6b51f3def812863
Iván Pérez <i.perezc.2019@alumnos.urjc.es>
2022-06-05 12:49:38 +0200

    Actualización a fase 3

---
+++ Juego/src/test/java/Juego/VolleyFighters/AppTest.java
@@ -0,0 +1,18 @@
+package Juego.VolleyFighters;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
